1fd5ba6b8d304f05e6b371e668f24675bfbddd22
Chris Clerville <glosseyvilly@hotmail.com>
2011-05-11 23:36:23 -0400

    Sanitize url for adding new new designer

--- src/intern/views.py
+++ src/intern/views.py
@@ -27,7 +27,7 @@ def AddDesigner(request):
         form = AddNewDesignerForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             clean = form.cleaned_data
-            developer = WebDesigner(designer=clean['name'],designer_url=clean['designer_url'],website_designed = clean['website_designed'])
+            developer = WebDesigner(designer=clean['name'],sanitizeUrl(designer_url=clean['designer_url']),website_designed = sanitizeUrl(clean['website_designed']))
             developer.save()
             return HttpResponseRedirect("thanks") # Redirect after POST
     else:
